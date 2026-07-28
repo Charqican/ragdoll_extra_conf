5d1080c83bfcc30b0bdc7c6e8a0d12360569a0c2
Charqican <david_7valenzuela@hotmail.com>
2026-07-28 02:50:11 -0400

    feat(tags) add compat tags as example

--- src/main/java/com/charqican/ragdollextraconf/registry/RagdollExConfTags.java
+++ src/main/java/com/charqican/ragdollextraconf/registry/RagdollExConfTags.java
@@ -11,8 +11,14 @@ import net.minecraft.world.level.block.Block;
 public class RagdollExConfTags {
 	public static final TagKey<Item> HEAVY_WEAPONS = itemTag("heavy_weapons");
 	public static final TagKey<Item> LIGHT_WEAPONS = itemTag("light_weapons");
-	public static final TagKey<Item> LIGHT_WEAPONS_MINECRAFT = itemTag("light_weapons/minecraft");
+
 	public static final TagKey<Item> HEAVY_WEAPONS_MINECRAFT = itemTag("heavy_weapons/minecraft");
+	public static final TagKey<Item> HEAVY_WEAPONS_TACZ = itemTag("heavy_weapons/tacz");
+	public static final TagKey<Item> HEAVY_WEAPONS_SIMPLY_SWORDS = itemTag("heavy_weapons/simply_swords");
+
+	public static final TagKey<Item> LIGHT_WEAPONS_MINECRAFT = itemTag("light_weapons/minecraft");
+	public static final TagKey<Item> LIGHT_WEAPONS_TACZ = itemTag("light_weapons/tacz");
+	public static final TagKey<Item> LIGHT_WEAPONS_SIMPLY_SWORDS = itemTag("light_weapons/simply_swords");
 
 	private static TagKey<Item> itemTag(String name) {
 		return TagKey.create(Registries.ITEM,
